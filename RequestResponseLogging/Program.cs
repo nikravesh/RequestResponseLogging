4e03fa19ee0bbfa119b815bf47928224eed3626a
Alireza Nikravesh <a.z.nikravesh@outlook.com>
2024-04-28 22:12:18 +0330

    used the middlewares

--- RequestResponseLogging/Program.cs
+++ RequestResponseLogging/Program.cs
@@ -1,3 +1,5 @@
+using RequestResponseLogging.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -15,6 +17,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseUnHandleException();
+app.UserRequestResponseLogging();
+
 
 app.UseHttpsRedirection();
 
